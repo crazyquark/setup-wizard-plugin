6e677223b0381117dabb853eef57323498f88e22
Abiy Seifu <abiy@ripple.com>
2014-08-18 15:01:35 -0700

    [TEST] wizard js/http api

--- test/wizard.js
+++ test/wizard.js
@@ -1,5 +1,4 @@
-var Wizard = require(__dirname+'/../../lib/wizard.js');
-var Client = require('ripple-rest-client');
+var Wizard = require(__dirname+'/../lib/wizard.js');
 
 describe('Wizard setup', function(){
   var wizard;
@@ -7,100 +6,10 @@ describe('Wizard setup', function(){
     wizard = new Wizard();
   });
 
-  it('should successfully account settings to', function(done){
-    var opts = {
-      ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
-      cold_wallet_secret: '<secret>'
-    };
-
-    wizard._updateAccountSettings(opts, function(err, response){
-      if (err) {
-        console.log('_updateAccountSettings err', err);
-      } else {
-        console.log('_updateAccountSettings success', response);
-      }
-      done();
-    });
-  });
-
-  it('should successfully issue currency with amount', function(done){
-    var opts = {
-      secret: '<secret>',
-      amount: 10,
-      currency: 'SWG',
-      destination_tag: 0
-    };
-
-    wizard._issueCurrency(opts, function(err, response){
-      if (err) {
-        console.log('_issueCurrency err', err);
-      } else {
-        console.log('_issueCurrency success', response);
-      }
-      done();
-    });
-
-  });
-
-  it('should successfully set trustline b/n cold and hot wallet for currency and amount', function(done){
-    this.timeout(10000);
-    var opts = {
-      currencies: {
-        "XAW": 10
-      }
-    };
-
-    wizard._setTrustLine(opts, function(err, response){
-      if (err) {
-        console.log('_setTrustLine err', err);
-      } else {
-        console.log('_setTrustLine success', response);
-      }
-      done();
-    });
-  });
-
-  it('should successfully add a single or multiple currencies', function(done){
-    this.timeout(30000);
-    var opts = {
-      secret: '<secret>',
-      destination_tag: 0,
-      currencies : {
-        'SWG': 1,
-        'XAW': 1,
-        'CAT': 22
-      }
-    };
-
-    wizard._addCurrency(opts, function(err, response){
-      if (err) {
-        console.log('_addCurrency err', err);
-      } else {
-        console.log('_addCurrency success', response);
-      }
-      done();
-    });
-  });
-
-  it('should successfully issue a single or multiple currencies', function(done){
-    this.timeout(10000);
-    var opts = {
-      secret: '<secret>',
-      amount: 10,
-      currency: 'XAW',
-      destination_tag: 0,
-      currencies : {
-        'SWG': 1,
-        'XAW': 1
-      }
-    };
-
-    wizard._issueCurrencies(opts, function(err, response){
-      if (err) {
-        console.log('_issueCurrency err', err);
-      } else {
-        console.log('_issueCurrency success', response);
-      }
+  it('should set a cold wallet', function(done){
+    wizard._setColdWallet('rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz', function(error, response){
+      console.log('error', error);
+      console.log('response', response);
       done();
     });
   });
