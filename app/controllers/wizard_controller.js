87cb2b18a305d1ee8008eee0316f29808576bcbf
Abiy Seifu <abiy@ripple.com>
2014-09-15 14:32:16 -0700

    [TASK] break controller

---
+++ app/controllers/wizard_controller.js
@@ -0,0 +1,44 @@
+const Wizard = require(__dirname+'/../lib/wizard');
+
+function WizardController (options) {
+  this.wizard = new Wizard({
+    gatewayd: options.gatewayd
+  });
+}
+
+WizardController.prototype = {
+  info: function(request, response) {
+    response.send({
+      success: true,
+      plugin: {
+        name: 'gatewayd-setup-plugin',
+        version: '0.1.0',
+        documentation: 'https://github.com/gatewayd/setup-wizard-plugin'
+      }
+    });
+  },
+  setup: function(request, response) {
+    var _this = this;
+    var options = request.body;
+
+    _this.wizard.setup(options, function(error, setup){
+      if (error) {
+        return response
+          .status(500)
+          .send({
+            success: false,
+            error: error
+        })
+      }
+
+      response
+        .status(200)
+        .send({
+          success: true,
+          setup: setup
+      });
+    });
+  }
+};
+
+module.exports = WizardController;
