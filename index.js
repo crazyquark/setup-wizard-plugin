c1f536b0c75f714114dcc65bc3058ff7393e8887
Abiy Seifu <abiy@ripple.com>
2014-09-15 19:37:36 -0700

    [FIX] export correct router

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var WizardController = require(__dirname+'/app/controllers/wizard_controller');
 
-
 function WizardPlugin (options) {
-  this.router = new express.Router();
+  var router = new express.Router();
 
   var wizardController = new WizardController({
     gatewayd: options.gatewayd
   });
 
-  this.router.use('/wizard', express.static(__dirname+'/public'));
-  this.router.get('/wizard/info', wizardController.info.bind(wizardController));
-  this.router.post('/wizard/setup', wizardController.setup.bind(wizardController));
+  router.get('/wizard/info', wizardController.info.bind(wizardController));
+  router.post('/wizard/setup', wizardController.setup.bind(wizardController));
+
+  router.use('/wizard', express.static(__dirname+'/public'));
+
+  this.router = router;
 }
 
 module.exports = WizardPlugin;
